281d83e1c8bb1a1592dd58335da6b2e5f7761c0e
evajbean <ejuenglingbean@bren.ucsb.edu>
2026-05-13 14:40:40 -0700

    Finishing up homework 4 and creating a biodiversity index function

---
+++ simpson_diversity.R
@@ -0,0 +1,15 @@
+# Simpson Diversity Function
+#
+# This function calculates the biodiversity index of a specific area, given the 
+# total number of species and the number of individuals in each species. Lower
+# index values are associated with lower diversity
+
+# param: n_i is the number of individuals of each species
+# param: N is the total number of species
+# return: diversity value (D)
+
+simpson.div <- function(n_i, N){
+  species.specific.div = (n_i/N)^2
+  div = sum(species.specific.div)
+  return(div)
+}
